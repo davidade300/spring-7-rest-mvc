ded2790634f060a3f3a32562df793ac9585416b9
davidade300 <iaderinho@icloud.com>
2026-01-28 18:25:54 -0300

    listBeer test, cutomer get and customer list tests

---
+++ src/test/java/com/david/spring7restmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,70 @@
+package com.david.spring7restmvc.controller;
+
+import com.david.spring7restmvc.model.Customer;
+import com.david.spring7restmvc.services.CustomerService;
+import com.david.spring7restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CustomerController.class)
+class CustomerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockitoBean
+    CustomerService customerService;
+
+    CustomerServiceImpl customerServiceImpl = new CustomerServiceImpl();
+
+    @Test
+    void updateBeerPatchById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void handlePost() {
+    }
+
+    @Test
+    void listCustomer() throws Exception {
+
+        given(customerService.listCustomers()).willReturn(customerServiceImpl.listCustomers());
+
+        mockMvc.perform(get("/api/v1/customer").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()", is(3)));
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+
+        Customer testCustomer = customerServiceImpl.listCustomers().getFirst();
+
+        given(customerService.getCustomerById(testCustomer.getId())).willReturn(testCustomer);
+
+        mockMvc.perform(get("/api/v1/customer/" + testCustomer.getId()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testCustomer.getId().toString())))
+                .andExpect(jsonPath("$.customerName", is(testCustomer.getCustomerName())));
+
+    }
+}
